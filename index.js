fc9d54c21dadf707e4c3512c63ec17004c01fb0d
SangWoo <sangw0804@likelion.org>
2018-10-31 01:28:59 +0900

    logging 추가

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
+const logger = require('./log');
 
 const { Chat } = require('./model/chat');
 
@@ -17,81 +18,82 @@ const server = http.createServer(app);
 const socketIO = require('socket.io');
 const io = socketIO(server);
 
-io.on('connection', socket => {
-  socket.on('join', (params, callback) => {
-    console.log('join!');
-    socket.join(params.reservationId);
-    // callback();
-  });
+try {
+  io.on('connection', socket => {
+    socket.on('join', (params, callback) => {
+      logger.info('join!');
+      socket.join(params.reservationId);
+      // callback();
+    });
 
-  socket.on('joinChat', (params, callback) => {
-    console.log('joinChat!');
-    socket.join(`${params.reservationId}_chat`);
+    socket.on('joinChat', (params, callback) => {
+      logger.info('joinChat!');
+      socket.join(`${params.reservationId}_chat`);
 
-    callback();
-  });
+      callback();
+    });
 
-  socket.on('leaveChat', (params, callback) => {
-    console.log('leaveChat!');
-    socket.leave(`${params.reservationId}_chat`);
-  });
+    socket.on('leaveChat', (params, callback) => {
+      logger.info('leaveChat!');
+      socket.leave(`${params.reservationId}_chat`);
+    });
 
-  socket.on('updateCheckpoint', async (params, callback) => {
-    let chat = await Chat.findById(params.reservationId);
-    console.log(chat);
+    socket.on('updateCheckpoint', async (params, callback) => {
+      let chat = await Chat.findById(params.reservationId);
 
-    const clients = io.sockets.adapter.rooms[`${params.reservationId}_chat`].sockets;
-    console.log(io.sockets.adapter.rooms);
-    const numClients = typeof clients !== 'undefined' ? Object.keys(clients).length : 0;
+      const clients = io.sockets.adapter.rooms[`${params.reservationId}_chat`].sockets;
+      const numClients = typeof clients !== 'undefined' ? Object.keys(clients).length : 0;
 
-    console.log(numClients);
-    chat.checkPoints[params.names[0]] = new Date().getTime();
+      chat.checkPoints[params.names[0]] = new Date().getTime();
 
-    if (numClients === 2) chat.checkPoints[params.names[1]] = new Date().getTime();
+      if (numClients === 2) chat.checkPoints[params.names[1]] = new Date().getTime();
 
-    chat.markModified('checkPoints');
-    await chat.save();
-    console.log(chat);
+      chat.markModified('checkPoints');
+      await chat.save();
+      logger.info(chat);
 
-    socket.to(params.reservationId).emit('newCheckPoints', { checkPoints: chat.checkPoints });
-    callback(chat.checkPoints);
-  });
+      socket.to(params.reservationId).emit('newCheckPoints', { checkPoints: chat.checkPoints });
+      callback(chat.checkPoints);
+    });
 
-  socket.on('getMessages', async (params, callback) => {
-    let chat = await Chat.findById(params.reservationId);
-    if (!chat) {
-      chat = await Chat.create({
-        _id: params.reservationId,
-        messages: [],
-        checkPoints
+    socket.on('getMessages', async (params, callback) => {
+      let chat = await Chat.findById(params.reservationId);
+      if (!chat) {
+        chat = await Chat.create({
+          _id: params.reservationId,
+          messages: [],
+          checkPoints
+        });
+      }
+
+      callback(chat.messages, chat.checkPoints);
+    });
+
+    socket.on('createMessage', async (params, callback) => {
+      const nowTime = new Date().getTime();
+      io.to(params.reservationId).emit('newMessage', {
+        content: params.content,
+        from: params.from,
+        to: params.to,
+        createdAt: nowTime
       });
-    }
+      const chat = await Chat.findById(params.reservationId);
+      chat.messages.push({
+        content: params.content,
+        from: params.from,
+        to: params.to,
+        createdAt: nowTime
+      });
+      await chat.save();
 
-    callback(chat.messages, chat.checkPoints);
-  });
-
-  socket.on('createMessage', async (params, callback) => {
-    const nowTime = new Date().getTime();
-    io.to(params.reservationId).emit('newMessage', {
-      content: params.content,
-      from: params.from,
-      to: params.to,
-      createdAt: nowTime
+      callback();
     });
-    const chat = await Chat.findById(params.reservationId);
-    chat.messages.push({
-      content: params.content,
-      from: params.from,
-      to: params.to,
-      createdAt: nowTime
-    });
-    await chat.save();
-
-    callback();
   });
-});
+} catch (e) {
+  logger.error(e);
+}
 
 const port = process.env.PORT || 3001;
 server.listen(port, () => {
-  console.log(`server is listening to :${port}`);
+  logger.info(`server is listening to :${port}`);
 });
