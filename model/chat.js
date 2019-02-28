e4fa87f31ca59d477b254cf7bf1db75255f6561c
SangWoo <sangw0804@likelion.org>
2019-02-28 15:12:35 +0900

    주석

--- model/chat.js
+++ model/chat.js
@@ -2,17 +2,20 @@ const mongoose = require('mongoose');
 
 const chatSchema = new mongoose.Schema(
   {
-    _id: mongoose.Schema.Types.ObjectId,
-    checkPoints: { type: mongoose.Schema.Types.Mixed, default: {} },
+    _id: mongoose.Schema.Types.ObjectId, // 해당 reservation의 _id 와 동일
+    checkPoints: { type: mongoose.Schema.Types.Mixed, default: {} }, // { "오상우": 1231133524, "신한결": 1231152413} 과 같이 마지막으로 확인한 시간을 타임스탬프로 기록
     user: {
+      // {name: "오상우", phoneNumber: "01087623725"}
       type: mongoose.Schema.Types.Mixed,
       default: {}
     },
     designer: {
+      // {name: "신한결", phoneNumber: "01012345678"}
       type: mongoose.Schema.Types.Mixed,
       default: {}
     },
     messages: [
+      // { content: "안녕", createdAt: 1234567890, from: "오상우", to: "신한결" }
       {
         content: String,
         createdAt: {
@@ -24,10 +27,12 @@ const chatSchema = new mongoose.Schema(
       }
     ],
     isValid: {
+      // 취소되었거나 완료되었다면 false
       type: Boolean,
       default: false
     },
     needAlarmTalk: {
+      // autoAlarmTalk.js 실행시 필요한 플래그 - 한번 알람톡이 보내진 경우에 중복해서 보내지 않도록 해줌.
       type: Boolean,
       default: true
     }
--- scripts/autoAlarmTalk.js
+++ scripts/autoAlarmTalk.js
@@ -5,12 +5,15 @@ const config = require('../config');
 const { Chat } = require('../model/chat');
 const logger = require('../log');
 
+// 매시 00분 / 30분 마다 chat 디비를 순회하면서 아직 완료되거나 취소되지 않은 예약의 chat을 찾아
+// if 마지막 메세지를 받은사람이 읽지 않은 경우 && 그 경우에 대해 알람톡을 보내지 않은 경우
+//    알람톡을 전송하고 알람톡을 보냈음을 needAlarmTalk 플래그로 표시한다.
+
 mongoose.connect(config.MONGO_DB_URL, { useNewUrlParser: true });
 
 const autoAlarmTalk = async () => {
   try {
     const chats = await Chat.find({ isValid: true });
-    console.log(chats);
 
     const promises = chats.map(async chat => {
       const lastMessage = chat.messages[chat.messages.length - 1];
